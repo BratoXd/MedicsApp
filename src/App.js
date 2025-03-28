7249e71136bd841b5a9070af8ecd0df22b35a697
BraulioHernandez13 <brauliocesar.hr@gmail.com>
2025-03-27 19:50:44 -0600

    se agrega ruta de formulario

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import MedicosList from './Components/MedicosList';
 import MedicoDetail from './Components/MedicDetail'; // Crear este componente
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
- 
+import DoctorForm from './Components/DoctorForm';
 
 function DoctorProfile() {
 
 
   return (
-     //    <div><DoctorForm /></div> 
+     
          
-        <Router>
+         <Router>
         <Routes>
             <Route path="/" element={<MedicosList />} />
             <Route path="/medico/:id" element={<MedicoDetail />} />
+            <Route path="/form" element={<DoctorForm />} />
         </Routes>
-    </Router>
+    </Router>  
    
   );
 }

ce41da91360ae1f0c04bcb94487f7635bf860aa5
BraulioHernandez13 <brauliocesar.hr@gmail.com>
2025-04-01 21:18:41 -0600

    se quita de medico list la llamada a google analitycs

--- src/Components/MedicosList.jsx
+++ src/Components/MedicosList.jsx
@@ -11,30 +11,7 @@ const MedicosList = () => {
             const response = await fetch('https://be-medicapp.onrender.com/medicos');
             const data = await response.json();
             setMedicos(data);
-            console.log("soydata 0 " + data[0])
-            if (Array.isArray(data) && data.length > 0) {
-                console.log("soydata 0", data[0]); // Para depuración
-                
-                if (data[0].campaña_activa && data[0].google_ads_id) {
-                    const script = document.createElement("script");
-                    script.async = true;
-                    script.src = `https://www.googletagmanager.com/gtag/js?id=${data[0].google_ads_id}`;
-                    document.head.appendChild(script);
-            
-                    const script2 = document.createElement("script");
-                    script2.innerHTML = `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', '${data[0].google_ads_id}');
-                    `;
-                    document.head.appendChild(script2);
-                }
-            } else {
-                console.error("Error: No se recibieron datos válidos de los médicos");
-            }
-            
-
+ 
             
         };
         fetchMedicos();
